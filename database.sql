7c80d2bdc61801790606a78a16056f23b037cab3
kanakova <114306804+kanakova@users.noreply.github.com>
2022-11-08 13:21:12 +0100

    aktualizovaná databáze
    doplněny tabulky heldeskový a recenzní formulář

--- database.sql
+++ database.sql
@@ -15,6 +15,61 @@ CREATE TABLE Users (
     PRIMARY KEY (UserID),
     UNIQUE (Username));
 
+-- vytvoření tabulky časopisů
+
+CREATE TABLE Journals (
+    JournalID int NOT NULL AUTO_INCREMENT,
+    Timestamp timestamp,
+    Issue int NOT NULL,
+    Volume year(4) NOT NULL,
+    Topic varchar(255),                                 -- téma: ekonomické vědy, společenské vědy, technické vědy, vědy o přírodě, vědy o člověku
+    PRIMARY KEY (JournalID));
+/* + data (PDF časopisu) */
+
+-- vytvoření tabulky článků
+
+CREATE TABLE Articles (
+    ArticleID int NOT NULL AUTO_INCREMENT,
+    Timestamp timestamp,
+    Title varchar(255),
+    Attribute varchar(255),                              -- stav: nový / odeslaný do recenzního řízení / k formálnímu doplnění / schválený / zamítnutý / hotový / vydaný
+    UserID int,
+    JournalID int,
+    PRIMARY KEY (ArticleID),
+    FOREIGN KEY (JournalID) REFERENCES Journals(JournalID)),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID));      -- id autora
+/* + data (PDF časopisu) */
+
+-- vytvoření tabulky oponentního formuláře
+
+CREATE TABLE Reviews (
+    ReviewID int NOT NULL AUTO_INCREMENT,
+    Timestamp timestamp,
+    UserID int,
+    Asset int,                                          -- aktuálnost, zajímavost a přínosnost
+    Originality int,
+    Expertness int,                                     -- odborná úroveň
+    Grammar int,                                        -- jazyková a stylistická úroveň
+    Text longtext,
+    Reference boolean,                                   -- doporučení (true = doporučuji / false = nedoporučuji)
+    ArticleID int NOT NULL,
+    PRIMARY KEY (ReviewID),
+    FOREIGN KEY (ArticleID) REFERENCES Articles(ArticleID)).
+    FOREIGN KEY (UserID) REFERENCES Users(UserID));     -- id recenzenta
+
+-- vytvoření tabulky helpdeskového formuláře
+
+CREATE TABLE Helpdesk (
+    HelpdeskID int NOT NULL AUTO_INCREMENT,
+    Timestamp timestamp,
+    Firstname varchar(255),
+    Lastname varchar(255),
+    Email varchar(255),
+    Telefon int,
+    Issue varchar(255),                                 -- Popis problému
+    Checkbox boolean,
+    PRIMARY KEY (HelpdeskID);
+
 -- přidání dat tabulky uživatelů
 
 INSERT INTO Users (Firstname, Lastname, Role) 
@@ -27,17 +82,6 @@ VALUES
     ('Hubert', 'Hartmann', 'Author'),
     ('Zlata', 'Zábranská', 'Author');
 
--- vytvoření tabulky časopisů
-
-CREATE TABLE Journals (
-    JournalID int NOT NULL AUTO_INCREMENT,
-    Timestamp timestamp,
-    Issue int NOT NULL,
-    Volume year(4) NOT NULL,
-    Topic varchar(255),
-    PRIMARY KEY (JournalID));
-/* + data (PDF časopisu) */
-
 -- přidání dat tabulky časopisů
 
 INSERT INTO Journals (Issue, Volume, Topic) 
@@ -48,16 +92,6 @@ VALUES
     (4, 2022, 'technické vědy'),
     (4, 2021, 'ekonomické vědy');
 
--- vytvoření tabulky článků
-
-CREATE TABLE Articles (
-    ArticleID int NOT NULL AUTO_INCREMENT,
-    Title varchar(255),
-    UserID int,
-    JournalID int NOT NULL,
-    PRIMARY KEY (ArticleID),
-    FOREIGN KEY (JournalID) REFERENCES Journals(JournalID)); 
-/* + data (PDF časopisu), stav, id redaktora a recenzenta */
 
 -- přidání dat tabulky článků
 
