1aca4a2209dce6613fe818781b27168235afb9f6
kanakova <114306804+kanakova@users.noreply.github.com>
2022-11-09 19:56:40 +0100

    GUI - dashboard - recenzent

--- js/datatables-simple-demo.js
+++ js/datatables-simple-demo.js
@@ -1,15 +1,20 @@
-window.addEventListener('DOMContentLoaded', event => {
+﻿window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
     // https://github.com/fiduswriter/Simple-DataTables/wiki
 
-    const tabulka = document.getElementById('tabulka');
-    if (tabulka) {
-        new simpleDatatables.DataTable(tabulka);
-    }
+    const datatablesSimple = document.getElementById('datatablesSimple');
+if (datatablesSimple) {
     
-    const search = document.getElementById('search');
-    if (search) {
-        new simpleDatatables.DataTable(search);
-    }
-});
+    new simpleDatatables.DataTable(datatablesSimple, {
 
+        labels: {
+            placeholder: "Hledat...", // The search input placeholder 
+            perPage: "{select} záznamů na stránku", // per-page dropdown label 
+            noRows: "Nebyly nalezeny žádné záznamy", // Message shown when there are no records to show 
+            noResults: "Vašemu vyhledávacímu dotazu neodpovídají žádné výsledky", // Message shown when there are no search results 
+            info: "Zobrazuje se {start} až {end} z {rows} záznamů" // 
+             
+        }
+      });
+}
+});
---
+++ reviewer.php
@@ -0,0 +1,68 @@
+﻿<?php
+require("connect.php");
+?>
+
+<!-- stránka -->
+<main>
+    <div class="container-fluid px-4">
+        <div class="card mb-4 mt-5">
+            <div class="card-header">
+                <i class="bi bi-table"></i> Přehled článků
+            </div>
+            <div class="card-body">
+<!-- tabulka -->
+    <?php
+            $condition = "nový"; // pouze nové články
+            $sql = "SELECT Articles.Title AS Title, CONCAT(Users.Firstname,' ',Users.Lastname) AS Author, Articles.Attribute AS Attribute FROM Articles JOIN Users ON Articles.UserID = Users.UserID WHERE Articles.Attribute LIKE '%".$condition."%'";
+            
+            $result = $conn->query($sql);
+            
+            if($result->num_rows > 0){
+                
+                echo "<table id='datatablesSimple'>
+                        <thead>
+                            <tr>
+                                <th>Název</th>
+                                <th>Autor</th>
+                                <th>Stav</th>
+                                <th>PDF</th>
+                                <th class='data-bs-toggle='tooltip' title='Oponentní formulář''>O.F.</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Název</th>
+                                <th>Autor</th>
+                                <th>Stav</th>
+                                <th>PDF</th>
+                                <th class='data-bs-toggle='tooltip' title='Oponentní formulář''>O.F.</th>
+                            </tr>
+                        </tfoot>";
+                    echo "<tbody>";
+                
+                while($row = $result->fetch_assoc()){
+
+                        echo "<tr>
+                                    <td>".$row["Title"]."</td>
+                                    <td>".$row["Author"]."</td>
+                                    <td>".$row["Attribute"]."</td>
+                                    <td><a href='assets/data/clanek_1.pdf' target='_blank'><img src='assets/img/PDF_icon.svg' class='icon'></a></td>";
+                                    echo"<td class='data-bs-toggle='tooltip' title='Oponentní formulář''><a href='#reviewform' data-bs-toggle='modal' data-bs-target='#reviewform'><img src='assets/img/form.svg' class='icon'></a></td>";
+                        echo "</tr>";
+                }
+                
+                    echo "</tbody>
+                </table>";
+
+            }
+            else{
+                echo "žádné výsledky";
+            }
+            
+
+    ?>
+<!-- konec tabulky -->
+            </div>
+        </div>
+    </div>
+</main>
