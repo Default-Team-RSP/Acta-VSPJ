8326c5478ab766aa2ba2ce8702f49794c6deae84
kanakova <114306804+kanakova@users.noreply.github.com>
2022-11-16 20:02:19 +0100

    admin.php = All.php; chief.php = editor.php

--- admin.php
+++ admin.php
@@ -2,6 +2,89 @@
 require("connect.php");
 ?>
 
-<?php
-echo "Toto bude stránka admina.";
-?>
+<!-- stránka -->
+<main>
+    <div class="container-fluid px-4">
+        <div class="card mb-4 mt-5">
+            <div class="card-header">
+                <i class="bi bi-table"></i> Přehled článků
+            </div>
+            <div class="card-body">
+<!-- tabulka -->
+    <?php
+            $sql = "SELECT t1.ArticleID, t1.Title, t1.Attribute, t1.Author, t2.Reviewer, Files.FileID FROM (SELECT Articles.ArticleID, Articles.Title, Articles.Attribute, CONCAT(Users.Firstname,' ',Users.Lastname) AS Author FROM Articles INNER JOIN Users ON Articles.UserID = Users.UserID WHERE Role = 'Author') t1 LEFT JOIN (SELECT Reviews.ArticleID, CONCAT(Users.Firstname,' ',Users.Lastname) AS Reviewer FROM Reviews INNER JOIN Users ON Reviews.UserID = Users.UserID) t2 ON (t1.ArticleID=t2.ArticleID) LEFT JOIN Files ON t1.ArticleID = Files.ArticleID";
+            
+            $result = $conn->query($sql);
+            
+            if($result->num_rows > 0){
+                
+                echo "<table id='datatablesSimple'>
+                        <thead>
+                            <tr>
+                                <th>Název</th>
+                                <th>Autor</th>
+                                <th>Recenzent</th>
+                                <th>Stav</th>
+                                <th>PDF</th>
+                                <th class='data-bs-toggle='tooltip' title='Zobrazit oponentní formulář''>O.F.</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Název</th>
+                                <th>Autor</th>
+                                <th>Recenzent</th>
+                                <th>Stav</th>
+                                <th>PDF</th>
+                                <th class='data-bs-toggle='tooltip' title='Zobrazit oponentní formulář''>O.F.</th>
+                            </tr>
+                        </tfoot>";
+                    echo "<tbody>";
+                
+                while($row = $result->fetch_assoc()){
+
+                        echo "<tr>
+                                    <td>".$row["Title"]."</td>
+                                    <td>".$row["Author"]."</td>";
+                                    $attr = $row['Attribute'];
+                                    if ($attr != 'nový') {
+                                          echo"<td>".$row["Reviewer"]."</td>";
+                                    } else {
+                                          echo"<td><select class='form-select' aria-label='Default select example'>
+  <option selected>Open this select menu</option>
+  <option value='1'>Recenzent 1</option>
+  <option value='2'>Recenzent 2</option>
+  <option value='3'>Recenzent 3</option>
+</select></td>";
+                                    }
+                                    echo"<td>".$row["Attribute"]."</td>";
+                                    $pdf = $row['FileID'];
+                                    if (is_null($pdf)) {
+                                          echo"<td></td>";
+                                    } else {
+                                          echo"<td><a href='get_file.php?id={$row['FileID']}'><img src='assets/img/PDF_icon.svg' class='icon'></a></td>";
+                                    }
+                                    $attr = $row['Attribute'];
+                                    if ($attr != 'nový' && $attr != 'odeslaný do recenzního řízení') {
+                                          echo"<td class='data-bs-toggle='tooltip' title='Zobrazit oponentní formulář''><a href='dashboard.php?link=showreview&id={$row['ArticleID']}'><img src='assets/img/form-done.svg' class='icon'></a></td>";
+                                    } else {
+                                          echo"<td></td>";
+                                    }
+                        echo "</tr>";
+                }
+                
+                    echo "</tbody>
+                </table>";
+
+            }
+            else{
+                echo "žádné výsledky";
+            }
+            
+
+    ?>
+<!-- konec tabulky -->
+            </div>
+        </div>
+    </div>
+</main>
--- chief.php
+++ chief.php
@@ -2,6 +2,97 @@
 require("connect.php");
 ?>
 
-<?php
-echo "Toto bude stránka šéfredaktora.";
-?>
+<!-- stránka -->
+<main>
+    <div class="container-fluid px-4">
+        <div class="card mb-4 mt-5">
+            <div class="card-header">
+                <i class="bi bi-table"></i> Přehled článků
+            </div>
+            <div class="card-body">
+<!-- tabulka -->
+    <?php
+            $condition = "nový"; //pouze články s příznakem "nový"
+            
+            //$sql1 = "SELECT t1.Title, t1.Attribute, t1.Author, t2.Reviewer FROM (SELECT Articles.ArticleID, Articles.Title, Articles.Attribute, CONCAT(Users.Firstname,' ',Users.Lastname) AS Author FROM Articles INNER JOIN Users ON Articles.UserID = Users.UserID WHERE Role = 'Author') t1 LEFT JOIN (SELECT Reviews.ArticleID, CONCAT(Users.Firstname,' ',Users.Lastname) AS Reviewer FROM Reviews INNER JOIN Users ON Reviews.UserID = Users.UserID) t2 ON (t1.ArticleID=t2.ArticleID) WHERE t1.Attribute LIKE '%".$condition."%'";
+            $sql1 = "SELECT t1.ArticleID, t1.Title, t1.Attribute, t1.Author, Files.FileID FROM (SELECT Articles.ArticleID, Articles.Title, Articles.Attribute, CONCAT(Users.Firstname,' ',Users.Lastname) AS Author FROM Articles INNER JOIN Users ON Articles.UserID = Users.UserID WHERE Role = 'Author') t1 LEFT JOIN (SELECT Reviews.ArticleID FROM Reviews INNER JOIN Users ON Reviews.UserID = Users.UserID) t2 ON (t1.ArticleID=t2.ArticleID) LEFT JOIN Files ON t1.ArticleID = Files.ArticleID WHERE t1.Attribute LIKE '%".$condition."%'";
+            $sql2 = "SELECT CONCAT(Users.Firstname,' ',Users.Lastname) AS Reviewer FROM Users WHERE Users.Role = 'Reviewer'";
+            
+            $result1 = $conn->query($sql1);
+            
+            if($result1->num_rows > 0){
+                
+                echo "<table id='datatablesSimple'>
+                        <thead>
+                            <tr>
+                                <th>Název</th>
+                                <th>Autor</th>
+                                <th>Recenzent</th>
+                                <th>Stav</th>
+                                <th>PDF</th>
+                                <th class='data-bs-toggle='tooltip' title='Oponentní formulář''>O.F.</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Název</th>
+                                <th>Autor</th>
+                                <th>Recenzent</th>
+                                <th>Stav</th>
+                                <th>PDF</th>
+                                <th class='data-bs-toggle='tooltip' title='Oponentní formulář''>O.F.</th>
+                            </tr>
+                        </tfoot>";
+                    echo "<tbody>";
+                
+                while($row = $result1->fetch_assoc()){
+
+                        echo "<tr>
+                                    <td>".$row["Title"]."</td>
+                                    <td>".$row["Author"]."</td>";
+                                    $attr = $row['Attribute'];
+                                    if ($attr != 'nový') {
+                                          echo"<td>".$row["Reviewer"]."</td>";
+                                    } else {
+                                          echo"<td>";
+                                          echo"<select class='form-select' aria-label='Default select example'>";
+                                          if ($result2->num_rows > 0) {
+                                              echo"<option selected>Recenzent</option>";
+                                              while($row = $result2->fetch_assoc()) {
+                                                echo"<option>".$row["Reviewer"]."</option>";
+                                              }
+                                              echo"</select>";
+                                          }
+                                          echo "</td>";
+                                    }
+                                    echo"<td>".$row["Attribute"]."</td>";
+                                    $pdf = $row['FileID'];
+                                    if (is_null($pdf)) {
+                                          echo"<td></td>";
+                                    } else {
+                                          echo"<td><a href='get_file.php?id={$row['FileID']}'><img src='assets/img/PDF_icon.svg' class='icon'></a></td>";
+                                    }
+                                    $attr = $row['Attribute'];
+                                    if ($attr != 'nový' && $attr != 'odeslaný do recenzního řízení') {
+                                          echo"<td class='data-bs-toggle='tooltip' title='Zobrazit oponentní formulář''><a href='#showreview' data-bs-toggle='modal' data-bs-target='#showreview'><img src='assets/img/form-done.svg' class='icon'></a></td>";
+                                    } else {
+                                          echo"<td></td>";
+                                    }
+                        echo "</tr>";
+                }
+                
+                    echo "</tbody>
+                </table>";
+
+            }
+            else{
+                echo "žádné výsledky";
+            }
+            
+
+    ?>
+<!-- konec tabulky -->
+            </div>
+        </div>
+    </div>
+</main>
